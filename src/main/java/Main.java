ad83cdf07e7e7c84f4877de993ce69cf59f48b44
ZANKRUT THAKKAR <zn834201@dal.ca>
2020-09-24 15:56:31 -0300

    initial project-setup and configurations

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        Main mainObj = new Main();
+        System.out.print(mainObj.printHello());
+    }
+    public String printHello(){
+        return "Hello Group 9!!!";
+    }
+}
