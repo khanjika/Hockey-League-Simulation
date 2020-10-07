c6159958c050b9ab896fb7bbac2c8193c66e594b
Arthy <arthy98@gmail.com>
2020-10-06 21:48:07 -0300

    Added Nested State Machine

---
+++ src/test/java/statemachine/NestedSimulatorTest.java
@@ -0,0 +1,4 @@
+package statemachine;
+
+public class NestedSimulatorTest {
+}
--- src/test/java/statesTest/CreateTeamStateTest.java
+++ src/test/java/statesTest/CreateTeamStateTest.java
@@ -1,5 +1,12 @@
 package statesTest;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 public class CreateTeamStateTest {
+    @Test
+    void entry(){
+
+    }
 
 }
