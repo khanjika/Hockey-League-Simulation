40a1549c425fe39385e574f89904a8b26acfebc5
Khanjika <kh779923@dal.ca>
2020-10-08 23:34:19 -0300

    Methods to connect to the stored procedure of division and team

--- src/main/java/conference/IConferencePersistent.java
+++ src/main/java/conference/IConferencePersistent.java
@@ -1,6 +1,5 @@
 package conference;
 
-
 public interface IConferencePersistent {
 
      int addConferenceInformation(String conferenceName, int leagueId);
---
+++ src/test/java/conference/ModelConferencePersistent.java
@@ -0,0 +1,23 @@
+package conference;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModelConferencePersistent {
+
+
+    int addConferenceInformation() {
+        return 1;
+    }
+
+
+    boolean isConferenceAlreadyExist() {
+        return true;
+    }
+
+
+    int getConferenceInformation() {
+        return 1;
+    }
+}
